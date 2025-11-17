59b90ccc02071312f1d1e45220aa68dada4c8eae
jaylord <jaylordmanila.coredev@gmail.com>
2025-11-17 15:30:44 +0800

    improve seo performace

---
+++ next-sitemap.config.ts
@@ -0,0 +1,9 @@
+import type { IConfig } from 'next-sitemap'
+
+const config: IConfig = {
+  siteUrl: 'https://jhaylhord.vercel.app',
+  generateRobotsTxt: true,
+  outDir: './public',
+}
+
+export default config
